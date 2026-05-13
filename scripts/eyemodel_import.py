cd613084a4aa7f039f4666b7e420f1758f69d732
LUMCNET\ljpors <l.j.pors@lumc.nl>
2026-05-13 16:57:30 +0200

    remove structure to create eye model, only support importing eye model data to existing eye model (easier to understand for user)

--- scripts/eyemodel_import.py
+++ scripts/eyemodel_import.py
@@ -15,6 +15,8 @@ logger = logging.getLogger(__name__)
 # --- user input
 
 import_method = "to_existing_eye_model"  # either 'to_existing_eye_model' or 'create_new_eye_model'
+new_eyemodel_name = "imported eye model"  # must be unique an non-empty
+new_eyemodel_description = "imported eye model"  # no requirements
 
 # --- end user input
 
@@ -22,22 +24,8 @@ logger.debug("commencing import")
 
 patient = ro_interface.load_current_patient()
 import_path = Config.IMPORT_PATH  # TODO: search the correct .json file from the directory
-
-# if import_method is 'to existing eye model', we need to load the eye model to which the imported data should be applied/ registered
-if import_method == "to_existing_eye_model":
-    structure_set = ro_interface.load_current_structureset()
-    geometry_generators, _ = ro_interface.load_eyemodel(structure_set=structure_set, eyemodelnr=Config.EYE_MODEL_NR)
-
-# if import_method is 'create new eye model, we should first create an eye model, and then obtain the structure_set and geometry_generators of that eye model
-elif import_method == "create_new_eye_model":
-    logger.debug("to implement now")  # temporary logger statement
-
-else:
-    raise NotImplementedError(
-        f'not implemented: importing eye model with method {import_method}\n\
-                              please enter "to_existing_eye_model" or "create_new_eye_model"'
-    )
-
+structure_set = ro_interface.load_current_structureset()
+geometry_generators, _ = ro_interface.load_eyemodel(structure_set=structure_set, eyemodelnr=Config.EYE_MODEL_NR)
 
 # import the eye model to the relevant structure_set and geometry_generators object
 
